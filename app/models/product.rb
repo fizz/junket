08c6639ef44958b1d8a6838c8817b0956be7379b
Fizz Orange <zizanio@me.com>
2016-09-23 02:23:48 -0500

    Tell product about sales
    Also validate attachment content type & price
    (Stripe minimum charge is 50 cents)

--- app/models/product.rb
+++ app/models/product.rb
@@ -27,4 +27,16 @@
 class Product < ApplicationRecord
   has_attached_file :file
   belongs_to :user
+  has_many :sales
+
+  validates_numericality_of :price,
+    greater_than: 49,
+    message: "must be at least 50 cents"
+
+  validates_attachment_content_type :file, :content_type => [
+      'image/jpg',
+      'image/jpeg',
+      'image/png',
+      'image/gif'
+    ]
 end
