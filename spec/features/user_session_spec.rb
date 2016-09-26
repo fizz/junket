d751ee2238f942af1f23009a64d57a0676c76cde
Fizz Orange <zizanio@me.com>
2016-09-25 20:45:30 -0500

    As a user, I can sign in and out!
    JUN-7
    JUN-8

--- spec/features/user_session_spec.rb
+++ spec/features/user_session_spec.rb
@@ -4,16 +4,19 @@ RSpec.feature "UserSession", type: :feature do
   let(:user) {FactoryGirl.create(:user)}
   scenario 'User visits the site to log in' do
     visit root_path
-    click_link 'Log In'
+    click_link 'Log in'
     fill_in_signin_fields
     expect(page).to have_content('Signed in successfully.')
   end
+
   scenario 'User logs out of the site' do
     visit root_path
+    click_link 'Log in'
     fill_in_signin_fields
     click_link 'Sign out'
     expect(page).to have_content('Signed out successfully.')
   end
+
   def fill_in_signin_fields
     fill_in "user_email", with: user.email
     fill_in "user_password", with: user.password
