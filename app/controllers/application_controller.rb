f0b7481b4483da4b23b5d77675a63b51236a97b5
Fizz Orange <zizanio@me.com>
2016-09-26 19:58:25 -0500

    Enforce Pundit policies
    JUN-10

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,10 +10,19 @@ class ApplicationController < ActionController::Base
 
   before_action :set_paper_trail_whodunnit
 
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to(request.referrer || root_path)
   end
 
 end
--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,38 +1,46 @@
 class ProfilesController < ApplicationController
-  before_action :set_user
   before_action :set_profile, only: [:show, :edit, :update, :destroy]
+  before_action :set_user
+  before_action :authenticate_user!
 
   respond_to :html
 
-  # def index
-  #   @profiles = Profile.all
-  #   respond_with(@profiles)
-  # end
+  def index
+    authorize @profile
+    @profiles = Profile.all
+    respond_with(@profiles)
+  end
 
   def show
+    authorize @profile
     respond_with(@user, @profile)
   end
 
   def new
     @profile = @user.build_profile
+    authorize @profile
     respond_with(@user, @profile)
   end
 
   def edit
+    authorize @profile
   end
 
   def create
     @profile = @user.create_profile(profile_params)
+    authorize @profile
     @profile.save
     respond_with(@user, @profile)
   end
 
   def update
+    authorize @profile
     @profile.update(profile_params)
     respond_with(@user, @profile)
   end
 
   def destroy
+    authorize @profile
     @profile.destroy
     respond_with(@profile)
   end
