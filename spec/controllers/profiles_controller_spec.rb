9d34659922f45d6ea7607828424bd5f1a5edafc2
Fizz Orange <zizanio@me.com>
2016-09-30 13:08:40 -0500

    remove duplicate before block

--- spec/controllers/profiles_controller_spec.rb
+++ spec/controllers/profiles_controller_spec.rb
@@ -24,10 +24,6 @@ RSpec.describe ProfilesController, type: :controller do
   # This should return the minimal set of attributes required to create a valid
   # Profile. As you add validations to Profile, be sure to
   # adjust the attributes here as well.
-  before do
-    visit new_user_session_path
-    sign_in(user)
-  end
 
   let(:user) { FactoryGirl.create(:user) }
 
@@ -38,17 +34,16 @@ RSpec.describe ProfilesController, type: :controller do
   let(:invalid_attributes) {
     {bio: nil, user_id: nil}
   }
-  before do
-    visit new_user_session_path
-    sign_in(user)
-  end
-
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
   # ProfilesController. Be sure to keep this updated too.
   let(:valid_session) { {} }
 
+  before do
+    visit new_user_session_path
+    sign_in(user)
+  end
   # describe "GET #index" do
   #   it "assigns all profiles as @profiles" do
   #     profile = Profile.create! valid_attributes
