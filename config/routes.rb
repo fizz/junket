693f9237a30d5878c54f491b7b1321100d52a30f
Fizz Orange <zizanio@me.com>
2016-09-30 19:45:02 -0500

    unnest some nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :users do
+  resources :users, shallow: true do
     resource :profile
     resources :hotels
   end
+
+  resources :hotels, only: :index
+  resource :profile, only: :show
 
   root 'home#index'
 end
